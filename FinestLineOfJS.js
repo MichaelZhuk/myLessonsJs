b3d4935de971132363e8ca3ccac79b49eee848b7
Michael Zhuk <michaelzhukluxlern@gmail.com>
2023-01-07 14:47:46 +0200

    07.01.23

--- FinestLineOfJS.js
+++ FinestLineOfJS.js
@@ -3,7 +3,7 @@ const family = ["Peter", "Ann", "Alex", "Linda"];
 function showFamily(arr) {
   if (family.length !== 0) {
     for (let index = 0; index < family.length; index++) {
-      let arr = '';
+      let arr = "";
       arr += family[index];
     }
     console.log(` Семья состоит из ${arr}`);
@@ -13,3 +13,15 @@ function showFamily(arr) {
 }
 
 showFamily(family);
+
+const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+
+function standardizeStrings() {
+  for (let index = 0; index < favoriteCities.length; index++) {
+    let arr = "";
+    arr += favoriteCities[index];
+    console.log(arr.toLowerCase());
+  }
+}
+
+standardizeStrings(favoriteCities);
